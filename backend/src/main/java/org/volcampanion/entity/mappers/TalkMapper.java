9c5c122ca3f7af0c85b8a3ca3c9d91ed17318ef2
Mickaël GERVAIS <mgervais@agaetis.fr>
2023-04-07 15:18:19 +0200

    :safety_vest: Add ignore on nested list to prevent recursive calls

--- backend/src/main/java/org/volcampanion/entity/mappers/TalkMapper.java
+++ backend/src/main/java/org/volcampanion/entity/mappers/TalkMapper.java
@@ -3,25 +3,29 @@ package org.volcampanion.entity.mappers;
 import static org.mapstruct.MappingConstants.ComponentModel.CDI;
 
 import java.util.List;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.volcampanion.domain.Talk;
 import org.volcampanion.entity.TalkEntity;
 
 @Mapper(componentModel = CDI,
-        uses = {
-                TalkFormatMapper.class,
-                TalkThemeMapper.class,
-                SpeakerMapper.class,
-                ConferenceMapper.class
-        })
+    uses = {
+        TalkFormatMapper.class,
+        TalkThemeMapper.class,
+        SpeakerMapper.class,
+        ConferenceMapper.class
+    })
 public interface TalkMapper extends IMapper<Talk, TalkEntity> {
 
-    Talk toDomain(TalkEntity entity);
+  @Mapping(target = "speakers", ignore = true)
+  Talk toDomain(TalkEntity entity);
 
-    TalkEntity toEntity(Talk domain);
+  @InheritInverseConfiguration
+  TalkEntity toEntity(Talk domain);
 
-    List<Talk> toDomain(List<TalkEntity> entity);
+  List<Talk> toDomain(List<TalkEntity> entity);
 
-    List<TalkEntity> toEntity(List<Talk> domain);
+  List<TalkEntity> toEntity(List<Talk> domain);
 
 }
