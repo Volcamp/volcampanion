7ab3482f7d02fc8a72658df210fcd75edb2375e1
rakhedair <rami.khedair@etu.uca.fr>
2023-04-18 10:27:04 +0200

    :bug: a rename issue of bottom-nav-bar

--- frontend/src/Components/bottom-nav-bar/bottom-nav-bar.component.ts
+++ frontend/src/Components/bottom-nav-bar/bottom-nav-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-bottem-nav-bar',
+  selector: 'app-bottom-nav-bar',
   templateUrl: './bottom-nav-bar.component.html',
   styleUrls: ['./bottom-nav-bar.component.sass']
 })
