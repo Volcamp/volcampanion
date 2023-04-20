538dbbefd5c08e6d8a562ba3c427ee27e0767748
rakhedair <rami.khedair@etu.uca.fr>
2023-04-20 09:25:31 +0200

    ✨ Add the date in the list of Talks

--- frontend/src/Components/talks-list/talks-list.component.ts
+++ frontend/src/Components/talks-list/talks-list.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Talk} from "../../Data/DTO/Talk"
 import {TalkPlan} from "../../Data/DTO/TalkPlan";
 import {Speaker} from "../../Data/DTO/Speaker";
 import {compareSchedule} from "../../GeneralVolcamp/CompareTalkPlan";
@@ -15,7 +14,7 @@ export class TalksListComponent implements OnInit{
   dictionaryViaLiteral = new Map<Date, Array<TalkPlan>>;
 
 
-speakerNames(speakers : Speaker[]) : string[]{
+  speakerNames(speakers : Speaker[]) : string[]{
     return speakers.map(speaker => speaker.name)
   }
 
@@ -25,32 +24,18 @@ speakerNames(speakers : Speaker[]) : string[]{
     let date : Date=this.talkPlans[0].schedule;
 
     for(let i=0;i<this.talkPlans.length;i++) {
-      console.log(this.talkPlans[i].talk.title)
-      console.log(this.talkPlans[i].schedule.getDate()+"/"+(this.talkPlans[i].schedule.getMonth()+1)+"/"+this.talkPlans[i].schedule.getFullYear())
-      console.log(date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear())
-      if(this.talkPlans[i].schedule!=date ){
+      if(this.talkPlans[i].schedule.getFullYear()!=date.getFullYear() || (this.talkPlans[i].schedule.getDate()!=date.getDate() || this.talkPlans[i].schedule.getMonth()!=date.getMonth())  ){
         this.dictionaryViaLiteral.set(date,listToAdd)
-        console.log("i pushed"+i)
         listToAdd=[]
         listToAdd.push(this.talkPlans[i])
         date=this.talkPlans[i].schedule
       }
       else{
-        console.log("i passed"+i)
         listToAdd.push(this.talkPlans[i])
       }
       if(i+1==this.talkPlans.length){
-        console.log("i finished"+i)
-
         this.dictionaryViaLiteral.set(date,listToAdd)
       }
-
-      console.log("----------------------------")
-    }
-    for(let i of this.dictionaryViaLiteral){
-      console.log("hey :"+i)
-
     }
   }
-
 }
