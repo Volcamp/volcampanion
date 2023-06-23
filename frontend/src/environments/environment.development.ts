2d5c6ed9981eb59c765d9ae62da8e1052766f260
rakhedair <rami.khedair@etu.uca.fr>
2023-06-23 18:49:09 +0200

    :lipstick: small change

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -3,7 +3,7 @@ import {LogLevel} from "angular-auth-oidc-client";
 export const environment = {
   production: false,
   // apiUrl: 'https://volcamp-api-dev.azurewebsites.net/',
-  apiUrl : 'http://192.168.50.10:8080/',
+  apiUrl : 'http://localhost:8080/',
   openIdClient: {
     authority: 'https://dev-stage-v.eu.auth0.com',
     redirectUrl: window.location.origin,
