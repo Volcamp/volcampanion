a47d1f968a555e9e7e09054f788a2eb7d766dd1b
Camille Petitalot <camillepetitalot@MacBook-Pro.local>
2023-09-18 11:36:43 +0200

    CR & Clean localstorage at strat up

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,11 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
 
+//Clean existing storage entry at startup
+Object.keys(localStorage)
+  .forEach((value, index) => {
+    localStorage.removeItem(value);
+  })
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
